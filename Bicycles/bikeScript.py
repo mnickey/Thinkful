69a5b0426cbfb9c8230c2156ae9c027106eb69aa
Michael Nickey <mnickey@gmail.com>
2014-09-25 21:18:25 -0700

    Added Pseudo Code to bikeScript.py

--- Bicycles/bikeScript.py
+++ Bicycles/bikeScript.py
@@ -21,12 +21,13 @@ if __name__ == '__main__':
 	""" Create 6 different bicycle models based on the imported Bicycle class """
 	inventory_list = []
 	inventory_list = [Bicycle("Roadster", 25, 150.00), \
-	Bicycle("Mountaineer", 50, 200), \
+	Bicycle("Mountaineer", 50, 160), \
 	Bicycle("Horizontal Hipster", 75, 400), \
 	Bicycle("Speedster", 25, 290), \
 	Bicycle("Trail Blazer", 45, 425), \
 	Bicycle("Single Forever Sitdowner", 65, 650)]
 	
+	# Not using the BikeShops class -- need to implement and add into a shop using the BikeShop class
 	print "Inventory"
 	print "-" * 20
 	for i in range(len(inventory_list)):
@@ -50,3 +51,24 @@ if __name__ == '__main__':
 			if inventory_list[x].prodCost < customer_list[i].cust_funds:
 				print "{0} can afford the {1}".format(customer_list[i].cust_name, inventory_list[x].modelName)
 	print '-' * 20
+
+	print '\nPurchasing'
+	print '-' * 20
+	for i in range(len(customer_list)):
+		for b in range(len(inventory_list)):
+			if inventory_list[b].shopCost <= customer_list[i].cust_funds:
+				print "{0} ---- {1} --- {2}".format(customer_list[i].cust_name, inventory_list[b].modelName, inventory_list[b].shopCost)
+				# --- Pseudo Code ---
+				# find the first item that the customer can purchase and buy it -- conditional if statement
+				
+				# remove the purchased bike from the inventor list -- del inventory_list[b]
+				# inventory_list.remove(inventory_list[b])
+
+				#instead of removing the bicycle from the list, set a flag in the class
+				
+				# remove the funds from the customer
+				# add the funds to the bike shop 
+				# re-loop over the bike inventory for the next customer
+				# remove the print statement above
+
+				
