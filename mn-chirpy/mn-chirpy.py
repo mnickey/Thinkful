9af54a68a3f09b83862d2713a710736fdaac4c20
Michael Nickey <mnickey@gmail.com>
2014-10-27 00:20:30 -0700

    added cmd line parser

---
+++ mn-chirpy/mn-chirpy.py
@@ -0,0 +1,49 @@
+# Imports for authorization by twitter
+import authorization
+import json
+import requests
+from urls import *
+
+# Imports for Argparse command line thingy
+import logging, csv
+import argparse
+import sys
+
+# Create logging output file and the log level.
+logging.basicConfig(filename="output.log", level=logging.DEBUG)
+
+# Creating the parser
+def make_parser():
+	""" Construct a command line parser """
+	logging.info("Creating Tweet Parser")
+	description = "Command line tool to create tweets"
+	parser = argparse.ArgumentParser(description=description)
+	subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+	# Creating post sub-parser
+	logging.debug("Creating Post Parser for Chirpy")
+	post_parser = subparsers.add_parser("post", help="post a tweet")
+	post_parser.add_argument("status", help="the message you want to post")
+
+# Main function
+def main():
+	""" Main function """
+	auth = authorization.authorize()
+	response = requests.get(TIMELINE_URL, auth=auth)
+	# print json.dumps(response.json(), indent=4)
+
+	# Since we're already logged on at this point I can use the 
+	# argparser to read the positional arguments and execute
+	# what the user wants to do...
+
+	# Command line parser here
+	parser = make_parser()
+	arguments = parser.parse_args()
+	# arguments = vars(arguments)
+	# command = arguments.pop("command")
+
+	if command:
+		print command 
+
+if __name__ == '__main__':
+	main()
