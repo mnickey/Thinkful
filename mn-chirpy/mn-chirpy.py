956a714fb7aebd8721d1ce747a454af67e4ee65e
Michael Nickey <mnickey@gmail.com>
2014-10-27 21:16:15 -0700

    added return statement

--- mn-chirpy/mn-chirpy.py
+++ mn-chirpy/mn-chirpy.py
@@ -22,14 +22,17 @@ def make_parser():
 
 	# Creating post sub-parser
 	logging.debug("Creating Post Parser for Chirpy")
-	post_parser = subparsers.add_parser("post", help="post a tweet")
-	post_parser.add_argument("status", help="the message you want to post")
+	post_parser = subparsers.add_parser("post", help="post a tweet to your account")
+	post_parser.add_argument("status", help="the message you want to post", nargs="?")
+	return parser
 
 # Main function
 def main():
 	""" Main function """
 	auth = authorization.authorize()
-	response = requests.get(TIMELINE_URL, auth=auth)
+	
+	# Uncomment these lines to show that the auth is actually there and working.
+	# response = requests.get(TIMELINE_URL, auth=auth)
 	# print json.dumps(response.json(), indent=4)
 
 	# Since we're already logged on at this point I can use the 
@@ -39,11 +42,11 @@ def main():
 	# Command line parser here
 	parser = make_parser()
 	arguments = parser.parse_args()
-	# arguments = vars(arguments)
-	# command = arguments.pop("command")
+	arguments = vars(arguments)
+	command = arguments.pop("command")
 
-	if command:
-		print command 
+	if command == "post":
+		print "This is the command named status", arguments
 
 if __name__ == '__main__':
 	main()
