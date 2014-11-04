cd31d49ff895e06c2fa0ab5c556517cf7ac66bd5
Michael Nickey <mnickey@gmail.com>
2014-11-03 20:24:22 -0800

    added show info ability

--- mn-chirpy/mn-chirpy.py
+++ mn-chirpy/mn-chirpy.py
@@ -9,6 +9,10 @@ import logging, csv
 import argparse
 import sys
 
+# Import pretty print
+from pprint import pprint as pp
+from pprint import pformat as pf
+
 # Create logging output file and the log level.
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
@@ -24,6 +28,14 @@ def make_parser():
 	logging.debug("Creating Post Parser for Chirpy")
 	post_parser = subparsers.add_parser("post", help="post a tweet to your account")
 	post_parser.add_argument("status", help="the message you want to post", nargs="?")
+	
+	# Creating show sub-parser
+	logging.debug("Creating show Parser for Chirpy")
+	show_parser = subparsers.add_parser("show", help="show a users information")
+	show_parser.add_argument("info", 
+						help="the user you want to show information about", 
+						nargs="?")
+
 	return parser
 
 # Main function
@@ -32,8 +44,8 @@ def main():
 	auth = authorization.authorize()
 	
 	# Uncomment these lines to show that the auth is actually there and working.
-	response = requests.get(TIMELINE_URL, auth=auth)
-	print json.dumps(response.json(), indent=4)
+	# response = requests.get(TIMELINE_URL, auth=auth)
+	# print json.dumps(response.json(), indent=4)
 
 	# Since we're already logged on at this point I can use the 
 	# argparser to read the positional arguments and execute
@@ -44,6 +56,7 @@ def main():
 	arguments = parser.parse_args()
 	arguments = vars(arguments)
 	command = arguments.pop("command")
+	print command 
 
 	if command == "post":
 		payload = arguments
@@ -51,6 +64,12 @@ def main():
 		print "URL: ", r.url
 		print "STATUS_CODE: ", r.status_code
 		print "TEXT: " + r.text
+	elif command =="show":
+		payload = arguments
+		r = requests.get(SHOW_URL, params=payload, auth=auth)
+		print "URL: ", r.url
+		print "STATUS_CODE: ", r.status_code
+		pp(r.json(), width=40, indent=2)
 	else:
 		print "No post message to send a tweet"
 
