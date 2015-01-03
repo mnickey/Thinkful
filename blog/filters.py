cad05daaf475ab38655f1c59fd491ad1728abdb6
Michael Nickey <mnickey@gmail.com>
2015-01-03 14:29:24 -0800

    Delete filters.py

--- blog/filters.py
+++
@@ -1,9 +0,0 @@
-from blog import app
-import mistune
-app.jinja_env.filters['markdown'] = mistune.markdown
-
-@app.template_filter()
-def dateformat(date, format):
-    if not date:
-        return None
-    return date.strftime(format)
